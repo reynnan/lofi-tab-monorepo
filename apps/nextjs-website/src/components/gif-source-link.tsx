477fe9c283d1d0c4b78e6835ae6ff163f4da7fab
Reynnan Viktor <reynnandev@gmail.com>
2025-03-08 16:15:28 +0100

    feat: Adding noreferrer to links

--- apps/nextjs-website/src/components/gif-source-link.tsx
+++ apps/nextjs-website/src/components/gif-source-link.tsx
@@ -4,7 +4,7 @@ import { useBackground } from "@/providers/background-provider";
 export default function GifSourceLink() {
   const { backgroundUrl } = useBackground();
   return (
-    <a href={backgroundUrl} target="_blank" className="font-bold">
+    <a href={backgroundUrl} rel="noreferrer" target="_blank" className="font-bold">
       Gif Source
     </a>
   );
